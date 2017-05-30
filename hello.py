95b65d703c7e6e4231dbfe2f1c44d1c559b4d4a9
codeyu <codeyu@gmail.com>
2017-05-30 10:46:53 +0800

    use flask-script

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask_script import Manager
 app = Flask(__name__)
-
+manager = Manager(app)
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
@@ -15,4 +16,4 @@ def user(name):
     return '<h1>Hello, %s!<h1>' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
